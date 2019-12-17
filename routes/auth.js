ac8aa73548911f2e5bb92fdeba376f11f83ee99b
Dmytro Grechka <tarqinn@ukr.net>
2019-12-17 18:18:10 +0200

    use auth token for access to user/routes, refresh tokens

--- routes/auth.js
+++ routes/auth.js
@@ -8,11 +8,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 router.post('/register', async (req, res) => {
-  const {
-    login,
-    pass,
-    role
-  } = req.body;
+  const { login, pass, role } = req.body;
 
   const hashedPass = await bcrypt.hash(pass, saltRounds).catch(err => err);
 
@@ -30,36 +26,73 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  const {
-    login,
-    pass
-  } = req.body;
+  const { login, pass } = req.body;
 
-  const currentUser = await AuthModel.findOne({login}).catch(err => err);
-  
+  const currentUser = await AuthModel.findOne({ login }).catch(err => err);
+
   if (!currentUser || currentUser.message) {
-    res.status(401).send({ message: 'login doesn\'t exist' });
+    res.status(401).send({ message: "login doesn't exist" });
     return;
   }
 
   const { _id, role } = currentUser;
   const result = await bcrypt.compare(pass, currentUser.pass);
-  const AutorizationToken = await jwt.sign({ _id, role }, Buffer.from(process.env.SECRET, 'base64'), { expiresIn: process.env.AUTH_TOKEN_LIFE });
-  const RefreshToken = await jwt.sign({ _id }, Buffer.from(process.env.SECRET, 'base64'), {expiresIn: process.env.REFRESH_TOKEN_LIFE });
+  const AutorizationToken = await jwt.sign(
+    { _id, role },
+    Buffer.from(process.env.SECRET, 'base64'),
+    { expiresIn: process.env.AUTH_TOKEN_LIFE }
+  );
+  const RefreshToken = await jwt.sign(
+    { _id },
+    Buffer.from(process.env.SECRET, 'base64'),
+    { expiresIn: process.env.REFRESH_TOKEN_LIFE }
+  );
 
-  const newToken = await new TokenModel({
-    token: RefreshToken
+  await new TokenModel({
+    token: RefreshToken,
+    userId: _id
   })
     .save()
     .catch(err => {
       console.log(err);
     });
-  
+
   if (result) {
-    res.status(200).send({AutorizationToken, RefreshToken, newToken});
+    res.status(200).send({ AutorizationToken, RefreshToken });
   } else {
     res.status(401).send({ message: 'password is incorrect' });
   }
+});
+
+router.post('/send-new-tokens', async (req, res) => {
+  const { refreshToken } = req.body;
+  const { _id } = jwt.decode(refreshToken);
+  const { role } = await AuthModel.findOne({ _id }).catch(err => err);
+  const AutorizationToken = await jwt.sign(
+    { _id, role },
+    Buffer.from(process.env.SECRET, 'base64'),
+    { expiresIn: process.env.AUTH_TOKEN_LIFE }
+  );
+  const RefreshToken = await jwt.sign(
+    { _id },
+    Buffer.from(process.env.SECRET, 'base64'),
+    { expiresIn: process.env.REFRESH_TOKEN_LIFE }
+  );
+  const result = await TokenModel.findOneAndDelete({
+    token: refreshToken
+  }).catch(err => err);
+  if (result && _id === result.userId && Date.now() < parseInt(exp + '000')) {
+    res.status(200).send({ AutorizationToken, RefreshToken });
+  } else {
+    res.status(401).send({ message: 'Invalid refresh token' });
+  }
+});
+
+router.post('/logout', async (req, res) => {
+  const { refreshToken } = req.body;
+  const { _id } = jwt.decode(refreshToken);
+  const userTokens = await TokenModel.deleteMany({ userId: _id });
+  userTokens ? res.status(200).send({message: 'Logout Successful'}) : res.status(401).send({ message: 'Invalid refresh token' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,27 +6,32 @@ const TokenModel = require('../models/token.model');
 const jwt = require('jsonwebtoken');
 
 router.use(async (req, res, next) => {
-  const { currentUser } = jwt.decode(req.headers.authorization);
-  const savedToken = await TokenModel.findOne({userId: currentUser._id});
-  console.log(savedToken);
-  next();
+  const currentUser = jwt.decode(req.headers.authorization);
+  const savedToken = await TokenModel.findOne({ userId: currentUser._id });
+  savedToken && Date.now() < parseInt(currentUser.exp + '000')
+    ? next()
+    : res.status(401).send({ message: 'Invalid auth token' });
 });
 
 router.get('/page/:skip', async function(req, res) {
-  const fullLength = await  UserModel.count();
-  
-  const userList = await UserModel.find({}).select(
-    'checked firstName lastName role businessLocation workEmail workPhone hourlyRate'
-  ).skip(parseInt(req.params.skip)).limit(5).sort({
-    _id: 'asc'
-  });
+  const fullLength = await UserModel.count();
+
+  const userList = await UserModel.find({})
+    .select(
+      'checked firstName lastName role businessLocation workEmail workPhone hourlyRate'
+    )
+    .skip(parseInt(req.params.skip))
+    .limit(5)
+    .sort({
+      _id: 'asc'
+    });
 
   if (!userList || userList.length === 0) {
     res.status(404).send({ message: 'Users not found' });
     return;
   }
 
-  return res.status(200).send({userList, fullLength});
+  return res.status(200).send({ userList, fullLength });
 });
 
 router.post('/create', async function(req, res) {
@@ -93,16 +98,16 @@ router.post('/remove/:id', async function(req, res) {
   return res.status(200).send(result);
 });
 router.get('/lalala', async function(req, res) {
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 /* GET users listing. */
 router.get('/:id', async function(req, res) {
   const { id } = req.params;
   try {
-    mongoose.Types.ObjectId(id)
-  } catch(err) {
-      res.sendStatus(404)
+    mongoose.Types.ObjectId(id);
+  } catch (err) {
+    res.sendStatus(404);
   }
   const user = await UserModel.findOne({ _id: id }).catch(err => err);
 
